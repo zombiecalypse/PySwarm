713c1994b63219807a97438422573dffb7919689
Aaron Karper <akarper@students.unibe.ch>
2012-03-19 19:08:47 +0100

    First Commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+from Swarm import version
+
+setup(
+	name = 'Swarm',
+	version = version,
+	description = 'TODO',
+	author = "None",
+	author_email = "None",
+	scripts = ['Swarm.py'],
+	packages = ['Swarm'],
+	install_requires = ["setuptools", ])
